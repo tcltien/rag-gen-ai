cc7527fabc4cc3c00d5b85b9d918604af7703a2c
TILE-DSK\Admin <atien.lh@gmail.com>
2026-03-03 18:37:41 +0700

    Initial commit

---
+++ test/genai-prj.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GenaiPrj from '../lib/genai-prj-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/genai-prj-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GenaiPrj.GenaiPrjStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
